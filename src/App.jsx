19df8b95468004b022f0b9e2e0f996086c09be8a
ankitkr52 <ankit72p@gmail.com>
2026-02-02 22:10:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SmoothScroll from './SmootScroll'
+
+const App = () => {
+  return (
+    <>
+      <SmoothScroll/>
+      <h1 className='bg-red-400 font-bold'>welcome</h1>
+     
+      <SmoothScroll/>
+    </>
+  )
+}
+
+export default App
