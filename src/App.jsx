1a20775854f1ed5178a48900be80ae83796cfedd
ankitkr52 <ankit72p@gmail.com>
2026-02-03 08:59:17 +0530

    fixed hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import SmoothScroll from './SmootScroll'
 import Header from './components/Header'
+import Hero from './section/Hero'
 
 const App = () => {
   return (
     <>
       <SmoothScroll />
       <Header />
-
+      <Hero />
       <SmoothScroll />
     </>
   )
