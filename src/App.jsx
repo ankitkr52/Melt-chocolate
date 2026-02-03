fa381e1a7b088808903d27969b787bd632923d90
ankitkr52 <ankit72p@gmail.com>
2026-02-03 09:07:20 +0530

    fixed animation issues

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import SmoothScroll from './SmootScroll'
 import Header from './components/Header'
 import Hero from './section/Hero'
+import Taste from './section/Taste'
+import Quality from './section/Quality'
+import Parallax from './section/Parallax'
 
 const App = () => {
   return (
@@ -9,6 +12,9 @@ const App = () => {
       <SmoothScroll />
       <Header />
       <Hero />
+      <Taste/>
+      <Quality/>
+      <Parallax/>
       <SmoothScroll />
     </>
   )
